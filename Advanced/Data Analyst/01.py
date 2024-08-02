4308d5db2ff54891b9095c42622e2371050cda25
Onkar Ijare <ijareonkar2184@gmail.com>
2024-08-02 13:48:15 +0530

    matplotlib

---
+++ Advanced/Data Analyst/01.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+x_axis = [1, 3, 5, 7]
+y_axis = [0, 5, 3, 2]
+
+plt.title("My Team")
+plt.scatter(x_axis, y_axis, color="red", marker="*", label="My Chart type")
+
+plt.xlabel("Single Point")
+plt.ylabel("New point")
+
+# plt.grid(True)
+plt.legend()
+plt.show()
