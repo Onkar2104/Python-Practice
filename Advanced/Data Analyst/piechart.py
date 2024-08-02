75da39543643981d1df9a17dcd44821e2035062c
Onkar Ijare <ijareonkar2184@gmail.com>
2024-08-02 14:55:58 +0530

    piechart

---
+++ Advanced/Data Analyst/piechart.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+ 
+labels = ['Oranges', 'Pears', 'Plums', 'Blueberries']
+quantity = [38, 45, 24, 10]
+
+colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
+
+plt.pie(quantity, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+
+plt.axis('equal')
+
+plt.show()
