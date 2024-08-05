a1e65415d043e8abae8f96d80435d9c6d505696a
Onkar Ijare <ijareonkar2184@gmail.com>
2024-08-05 12:44:20 +0530

    seaborn

---
+++ Advanced/DataAnalyst/seaborn6.py
@@ -0,0 +1,8 @@
+#Seaborn is a library that uses Matplotlib underneath to plot graphs.
+# It will be used to visualize random distributions.
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot([0, 1, 7, 3, 4, 15])
+
+plt.show()
