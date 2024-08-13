763e209603ae7400f16bb69cfc20bebba0d5c3fa
Onkar Ijare <ijareonkar2184@gmail.com>
2024-08-13 13:43:18 +0530

    database

--- Django/core/home/models.py
+++ Django/core/home/models.py
@@ -10,4 +10,11 @@ class student(models.Model):
     address = models.TextField()
 
 
-# class product()
+class product(models.Model):
+    name = models.CharField(max_length=10)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+
+class fan(models.Model):
+    name = models.CharField(max_length=20)
