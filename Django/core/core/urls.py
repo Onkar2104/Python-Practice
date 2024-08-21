6b69258dda65d39eface5d0793d0896ee986b753
Onkar Ijare <ijareonkar2184@gmail.com>
2024-08-21 22:11:20 +0530

    receipes

--- Django/core/core/urls.py
+++ Django/core/core/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import *
+from vege.views import *
 
 urlpatterns = [
     path('', home, name="home"),
-    # path('index/', contact, name="index"),
+    path('receipes/', receipes, name="receipes"),
     path('contact/', contact, name="contact"),
     path('about/', about, name="about"),
     
