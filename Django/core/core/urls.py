c535be79456bd61155349da5a28a188c49848ceb
Onkar Ijare <ijareonkar2184@gmail.com>
2024-08-23 16:55:13 +0530

    search & update functionality

--- Django/core/core/urls.py
+++ Django/core/core/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', home, name="home"),
     path('receipes/', receipes, name="receipes"),
     path('delete_receipe/<id>/', delete_receipe, name="delete_receipe"),
+    path('update_receipe/<id>/', update_receipe, name="update_receipe"),
     path('contact/', contact, name="contact"),
     path('about/', about, name="about"),
     
